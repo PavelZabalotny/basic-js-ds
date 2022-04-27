2b669353c515df1cf5c2dc8f8ba319f6844b7cb0
PavelZabalotny <pavelzabalotny@gmail.com>
2022-04-27 23:32:42 +0300

    feat: implement remove-from-list.js

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+	if (!l) {
+		return null
+	}
+
+	while (l.value === k) {
+		l = l.next
+	}
+
+	let cur = l
+	while (cur.next) {
+		if (cur.next.value === k) {
+			cur.next = cur.next.next
+		} else {
+			cur = cur.next
+		}
+	}
+	return l
 }
 
 module.exports = {
-  removeKFromList
+	removeKFromList
 };
